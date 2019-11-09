dd1605411758b3255e775a2bd4a815ca26fbe03e
Djamilson <djamilson@gmail.com>
2019-11-09 00:06:22 -0300

    Mensagem de logout

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,6 +3,22 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   flex: 1;
 `;
+export const Heder = styled.View`
+  align-self: center;
+  align-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Icons = styled.View`
+  align-content: center;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-direction: row;
+  margin-top: 35px;
+  width: 70px;
+  margin-left: 20px;
+`;
 
 export const Title = styled.Text`
   font-size: 20px;
@@ -10,6 +26,9 @@ export const Title = styled.Text`
   font-weight: bold;
   align-self: center;
   margin-top: 30px;
+  margin-left: 50px;
+  padding-left: 70px;
+  padding-right: 40px;
 `;
 
 export const List = styled.FlatList.attrs({
