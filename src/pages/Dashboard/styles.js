2bd997cf1e736d732a9f72092d6c534cc804999a
Djamilson <djamilson@gmail.com>
2019-11-14 13:46:10 -0300

    New server

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -4,20 +4,23 @@ export const Container = styled.SafeAreaView`
   flex: 1;
 `;
 export const Heder = styled.View`
+  fex: 1;
   align-self: center;
   align-content: space-between;
   align-items: center;
   flex-direction: row;
+  border: solid 2px #000;
 `;
 
 export const Icons = styled.View`
+  flex: 1;
   align-content: center;
   justify-content: space-between;
   align-items: flex-start;
   flex-direction: row;
   margin-top: 35px;
-  width: 70px;
   margin-left: 20px;
+  max-width: 90px;
 `;
 
 export const Title = styled.Text`
@@ -26,9 +29,9 @@ export const Title = styled.Text`
   font-weight: bold;
   align-self: center;
   margin-top: 30px;
-  margin-left: 50px;
-  padding-left: 70px;
-  padding-right: 40px;
+  padding-right: 50px;
+
+  border: solid 2px #000;
 `;
 
 export const List = styled.FlatList.attrs({
